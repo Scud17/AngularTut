016143adae84ff74a39e05b19060393534e18cec
Patrick Savella <patricksavella17@gmail.com>
2022-11-18 15:04:42 -0500

    passing params to components

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   // selector: 'pm-product-detail',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail"
-  constructor() { }
+  product: IProduct | undefined;
+  
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    this.pageTitle += `: ${id}`;
+    this.product = {
+        "prodid": 5,
+        "prodName": "Hammer",
+        "prodCode": "TBX-0048",
+        "releaseDate": "May 11, 2021",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imgUrl": "assets/images/me.jpg"
+    }
   }
 
+  onBack(): void{
+    this.router.navigate(['/products']);
+  }
 }
