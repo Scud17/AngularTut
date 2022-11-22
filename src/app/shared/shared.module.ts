daf2ec2b4e0561eeed8bfc6e965c19871077ab22
Patrick Savella <patricksavella17@gmail.com>
2022-11-21 21:12:49 -0500

    Refactoring to multiple modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StarComponent } from './star.component';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './conver-to-spaces.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    StarComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    StarComponent,
+    ConvertToSpacesPipe,
+    CommonModule,
+    FormsModule
+  ]
+})
+export class SharedModule { }
